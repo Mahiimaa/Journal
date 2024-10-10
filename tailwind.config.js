b4a501c60a1b2d4ec179e94df6a4df67956883e0
Mahiimaa <137261863+Mahiimaa@users.noreply.github.com>
2024-10-10 21:02:28 +0545

    css updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}",
+    './static/**/*.css', // If you have any CSS files that use Tailwind classes
+    './static/**/*.js',  // If you use Tailwind classes in your JavaScript files
+    './**/*.html' 
+  ],
+  
   theme: {
     extend: {},
   },
